33274dd49cac6c6020906e82a2fc7cca62a55581
pranshuPant07 <pranshupantofficial@gmail.com>
2024-10-04 10:35:10 +0530

    Initial Changes

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Style/Home.css'
+
+function Home() {
+  return (
+    <div>
+        <h1 style={{"textAlign":"center"}}>HELLO</h1>
+    </div>
+  )
+}
+
+export default Home
